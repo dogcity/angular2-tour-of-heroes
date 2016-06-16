2be628af6105cea76564653efcbb9a00b25fe064
José Ortega <jose.ortega.soto@gmail.com>
2016-06-16 16:19:47 -0400

    clon inicial del proyecto, por problemas de la continuación del tutorial

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent);
-=======
-import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
-
-bootstrap(AppComponent);
->>>>>>> cap-3
--- e2e-spec.js
+++ e2e-spec.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 describe('QuickStart E2E Tests', function () {
 
@@ -14,20 +13,3 @@ describe('QuickStart E2E Tests', function () {
   });
 
 });
-=======
-
-describe('QuickStart E2E Tests', function () {
-
-	var expectedMsg = 'My First Angular 2 App';
-
-
-  beforeEach(function () {
-    browser.get('');
-  });
-
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
-  });
-
-});
->>>>>>> cap-3
